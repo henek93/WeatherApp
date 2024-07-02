b087f2d415056c5d51dade6a849750bf7896214d
henek93 <sludnikov.slava03@gmail.com>
2024-07-02 13:18:34 +0300

    изменил метод подстановки ключа в api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,12 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+
+        val key = property("apikey")?.toString() ?: error(
+            "You should add apikey into gradle" +
+                    ".properties"
+        )
+        buildConfigField("String", "WEATHER_API_KEY", "\"$key\"")
     }
 
     buildTypes {
@@ -40,6 +46,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.10"
